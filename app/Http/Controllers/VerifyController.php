e5d87e99e80d71e176a968d30b58e570115786e2
Guilherme Costa <guicm007@gmail.com>
2023-01-12 17:46:15 -0300

    feat: create verify function in controller

--- app/Http/Controllers/VerifyController.php
+++ app/Http/Controllers/VerifyController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\VerifyFormRequest;
+use App\Http\Resources\VerifyResource;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
 
 class VerifyController extends Controller
 {
-    //
+    private array $response = [];
+    private int $statusCode = Response::HTTP_OK;
+
+    /**
+     * Method that verify if password is valid.
+     *
+     * @param VerifyFormRequest $request
+     * @return VerifyResource
+     */
+    public function logout(VerifyFormRequest $request)
+    {
+        try {
+            $this->response = $request;
+        } catch (Throwable $e) {
+            Log::error(__CLASS__ . '::' . __FUNCTION__ . ', ' . $e->getMessage());
+
+            $this->response = [
+                'message' => [trans('validation.bad_request')]
+            ];
+            $this->statusCode = Response::HTTP_BAD_REQUEST;
+        }
+
+        return (new VerifyResource($this->response))
+            ->response()->setStatusCode($this->statusCode);
+    }
 }
