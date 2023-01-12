fcde43388b5382345e5f2ece7e8cf0cadf64861d
Guilherme Costa <guicm007@gmail.com>
2023-01-12 17:11:42 -0300

    feat: add route /verify, create pt-br lang file

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VerifyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(VerifyController::class)->group(function () {
+    Route::post('/verify', 'verify')->name('api.verify');;
 });
