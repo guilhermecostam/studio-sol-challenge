e3a89327cea1deaa5c6dd1a79aa1d2c7ed690ac2
Guilherme Costa <guicm007@gmail.com>
2023-01-13 10:48:22 -0300

    adjust: disable data wrapping in resource

--- app/Http/Resources/VerifyResource.php
+++ app/Http/Resources/VerifyResource.php
@@ -53,6 +53,11 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class VerifyResource extends JsonResource
 {
     /**
+     * Disable data wrapping
+     */
+    public static $wrap = null;
+
+    /**
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
